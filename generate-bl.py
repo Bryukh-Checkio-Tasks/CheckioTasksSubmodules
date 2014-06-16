764d0a832d428e6b7167064b8cfb3977c94aac2f
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-06-16 17:35:52 +1200

    tests

--- generate-bl.py
+++ generate-bl.py
@@ -9,6 +9,39 @@ def human2bird(phrase):
         result.append("".join(ch * 3 if ch in VOWELS else ch + random.choice(VOWELS) for ch in w))
     return " ".join(result)
 
-print(human2bird("checkio"))
-print(human2bird("checkio"))
-print(human2bird("a b c d e f"))
+def tranlate(phrase):
+    words = phrase.split()
+    result = []
+    for w in words:
+        i = 0
+        temp = ""
+        while i < len(w):
+            temp += w[i]
+            if w[i] in VOWELS:
+                i += 3
+            else:
+                i += 2
+        result.append(temp)
+    return " ".join(result)
+
+# print(human2bird("checkio"))
+# print(human2bird("checkio"))
+# print(human2bird("a b c d e f"))
+
+print(tranlate("cuhueeecikuiiiooo"))
+
+T = [
+    "lorem ipsum",
+    "to be or not to be",
+    "bla bla bla bla",
+    "do you speak english",
+    "i don not understand you"
+
+]
+
+for t in T:
+    b = human2bird(t)
+    print("""{{
+    "input": "{}",
+    "answer": "{}"
+    }},""".format(b, t))
